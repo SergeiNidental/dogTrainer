fc7ae3a9336edf925001f86823a595d065caf8b8
SergeiNidental <sergeinidental@gmail.com>
2023-11-21 07:44:33 -0500

    Made Text more felxibal

--- src/scripts/components/text.jsx
+++ src/scripts/components/text.jsx
@@ -5,24 +5,20 @@ import '../../styles/text/__text_name';
 import '../../styles/text/__text_nameDiscription';
 import '../../styles/text/__text_diploms';
 
-export default function Text({id, children}){
+export default function Text({typeTag, id, children}){
 let classCase;
 
 switch (id) {
     case 'name':
-        classCase='name';
+        classCase={className:'name'};
         break;
     case 'nameDiscription':
-        classCase='nameDiscription';
+        classCase={className:'nameDiscription'};
         break;
     case 'diploms':
-        classCase='diploms';
+        classCase={className:'diploms'};
         break;
 }
 
-    return(
-        <p className={classCase}>
-            {children}
-        </p>
-    );
+    return React.createElement(typeTag,classCase, children);
 }
