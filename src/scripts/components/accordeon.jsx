34cc4ab913e20beba136b3d752b56d78f5b3dac9
SergeiNidental <sergeinidental@gmail.com>
2023-11-20 20:05:39 -0500

    Accordeon was added

---
+++ src/scripts/components/accordeon.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Accordeon({children}){
+    
+    return (
+        <>
+        <button>ПОДРОБНЕЕ ОБО МНЕ</button>
+        <div>
+            <p>
+                
+            </p>
+        </div>
+        </>
+    )
+}
