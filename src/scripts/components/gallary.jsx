79ac7167d7f145608cbf952aa7dbd296c9edfa35
SergeiNidental <sergeinidental@gmail.com>
2023-12-29 12:18:43 -0500

    Gallary size fixed

--- src/scripts/components/gallary.jsx
+++ src/scripts/components/gallary.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 import img2 from '../../img/2.PNG';
+import img1 from '../../img/1.PNG';
+import img3 from '../../img/3.PNG';
 
 import '../../styles/img/__img_1';
 
@@ -9,7 +11,9 @@ import Img from '../components/img';
 export default function Gallary(){
     return (
       <div className="scroll-container">
-        <Img src={img2} alt='gallary'  />
+        <Img src={img2} alt='gallary' />
+        <Img src={img1} alt='gallary' />
+        <Img src={img3} alt='gallary' />
       </div>  
     );
 }
