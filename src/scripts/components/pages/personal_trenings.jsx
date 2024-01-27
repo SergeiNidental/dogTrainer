8d2c61ffcbb18bd20165d244b5c0d79a145281bf
SergeiNidental <sergeinidental@gmail.com>
2024-01-27 15:02:31 -0500

    Titre pers trening added

--- src/scripts/components/pages/personal_trenings.jsx
+++ src/scripts/components/pages/personal_trenings.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
+import Text from "../text";
+
 export default function PersonalTrenings(){
     return(
         <>
+        <Text typeTag='h1' id='name'>
+            Очные занятие в Монреале
+        </Text>
         </>
     )
 }
