32c4a44a0557358a036e0a3bdf33924b930874a3
SergeiNidental <sergeinidental@gmail.com>
2024-01-27 15:15:34 -0500

    What you get pers tren added

--- src/scripts/components/pages/personal_trenings.jsx
+++ src/scripts/components/pages/personal_trenings.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import Text from "../text";
 import Img from '../img';
+import ListComponent from "../list";
+import Container from "../container";
 
 import banner from '../../../img/banner.jpg';
 
@@ -15,6 +17,13 @@ export default function PersonalTrenings(){
         <Text typeTag='h2' id='reviews'>
         С КАКИМИ ПРОБЛЕМАМИ Я МОГУ ВАМ ПОМОЧЬ?
       </Text>
+      <ListComponent id='problemToResolve' />
+      <Container id='backgroundBrown'>
+      <Text typeTag='h2' id='backgroundBrown'>
+        ЧТО ВЫ ПОЛУЧИТЕ?
+      </Text>
+      <ListComponent id='whatyouwillget' />
+      </Container>
         </>
     )
 }
